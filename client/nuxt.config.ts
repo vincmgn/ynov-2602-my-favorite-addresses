874d14f2a29c6bc87494ef1bee9840055e54759f
Vincent Magnien <vincentmagnienpro@gmail.com>
2026-02-24 09:11:01 +0100

    feat: user pinia

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "@pinia/nuxt"],
   devServer: {
     port: 3333,
   },
